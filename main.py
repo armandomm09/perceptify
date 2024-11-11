acfac1ea1785b9b4cee5d3a85c58593ee9a30f77
Pablo Armando Mac Beath Milian <pabloarmando.macbeath@gmail.com>
2024-11-10 19:44:57 -0600

    Fall detector class

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ import cv2
 from ultralytics import YOLO
 from constants import *
 from training import continue_training, train_from_scratch
-from process import analyze_video_folder, analyze_realtime, analyze_native_yolo, analyze_video
 from utils import rename_and_order_files
 import os
 from process import FallDetector
@@ -12,7 +11,7 @@ from database import PSQLManager
 
 manager = PSQLManager(DATABASE_CONFIG_PATH)
 detector = FallDetector(TRAINED_MODEL_BEST_PATH, YOLO_V_11_PATH, manager)
-detector.analyze_video("media/in_videos/video7.mp4", "media/out_videos/video7.mp4", save=True, open_in_finder=True)
+# detector.analyze_video("media/in_videos/video7.mp4", "media/out_videos/video7.mp4", save=True, open_in_finder=True)
 
 results = manager.get_all_cv_fall_readings()
 
